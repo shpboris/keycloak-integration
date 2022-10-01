58adcf86e7e0ef539f7ef436f95c4b95b338b824
shpilb <shpilb@Dell.com>
2022-10-01 13:57:18 +0300

    Added interceptor - initial change

--- auth-code-client/client.go
+++ auth-code-client/client.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"github.com/shpboris/logger"
 	"golang.org/x/oauth2"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
 var (
-	config = oauth2.Config{
+	idToTokenMap = make(map[string]*oauth2.Token)
+	config       = oauth2.Config{
 		ClientID:     "keycloak-integration-app",
 		ClientSecret: "BnpvHny0VkN8k1DaTKykr7KzdYJCpKyX",
 		//Scopes:       []string{"all"},
@@ -26,28 +32,46 @@ var (
 )
 
 func main() {
-
-	// 1 - We attempt to hit our Homepage route
-	// if we attempt to hit this unauthenticated, it
-	// will automatically redirect to our Auth
-	// server and prompt for login credentials
-	http.HandleFunc("/", HomePage)
-
-	// 2 - This displays our state, code and
-	// token and expiry time that we get back
-	// from our Authorization server
-	http.HandleFunc("/oauth2", Authorize)
-
-	// 3 - We start up our Client on port 9094
+	r := mux.NewRouter()
+	r.HandleFunc("/", homePage)
+	r.HandleFunc("/nested", nestedPage)
+	r.HandleFunc("/oauth2", Authorize)
+	r.Use(HeaderMiddleware)
 	log.Println("Client is running at 9094 port.")
-	log.Fatal(http.ListenAndServe(":9094", nil))
+	log.Fatal(http.ListenAndServe(":9094", r))
 }
 
-// Homepage
-func HomePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Homepage Hit!")
-	u := config.AuthCodeURL("xyz")
-	http.Redirect(w, r, u, http.StatusFound)
+func homePage(w http.ResponseWriter, r *http.Request) {
+	b, err := os.ReadFile("./static/home.html")
+	if handlePossibleError(err, w) {
+		return
+	}
+	w.Header().Set("Content-type", "text/html")
+	_, err = w.Write(b)
+	handlePossibleError(err, w)
+}
+
+func nestedPage(w http.ResponseWriter, r *http.Request) {
+	b, err := os.ReadFile("./static/nested.html")
+	if handlePossibleError(err, w) {
+		return
+	}
+	w.Header().Set("Content-type", "text/html")
+	_, err = w.Write(b)
+	handlePossibleError(err, w)
+}
+
+func HeaderMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.RequestURI, "/oauth") {
+			ck, _ := r.Cookie("SESSION_ID")
+			if ck == nil || !validateOrRefreshToken(ck.Value) {
+				u := config.AuthCodeURL("xyz")
+				http.Redirect(w, r, u, http.StatusFound)
+			}
+		}
+		next.ServeHTTP(w, r)
+	})
 }
 
 // Authorize
@@ -71,7 +95,34 @@ func Authorize(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	e := json.NewEncoder(w)
-	e.SetIndent("", "  ")
-	e.Encode(*token)
+	id := uuid.New().String()
+	idToTokenMap[id] = token
+
+	expires := time.Now().AddDate(0, 0, 1)
+	ck := http.Cookie{
+		Name:    "SESSION_ID",
+		Domain:  "localhost",
+		Path:    "/",
+		Expires: expires,
+	}
+	ck.Value = id
+	http.SetCookie(w, &ck)
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func validateOrRefreshToken(id string) bool {
+	token := idToTokenMap[id]
+	msg := fmt.Sprintf("Found token: %s", token.AccessToken)
+	logger.Log.Info(msg)
+	return true
+}
+
+func handlePossibleError(err error, w http.ResponseWriter) bool {
+	if err != nil {
+		logger.Log.Error(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return true
+	}
+	return false
 }
